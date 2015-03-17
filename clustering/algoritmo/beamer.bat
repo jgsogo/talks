26987e52f09ca0cd08aad73ca7096f92e53bdabb
Javier G. Sogo <jgsogo@gmail.com>
2015-03-17 13:16:39 +0100

    diapo final en algoritmos

--- clustering/algoritmo/beamer.bat
+++ clustering/algoritmo/beamer.bat
@@ -1,8 +1,21 @@
+:: Copy images to this directory
+pushd ..
+pushd ..
+pushd common
+copy github.png "../clustering/algoritmo/github.png"
+copy twitter.png "../clustering/algoritmo/twitter.png"
+copy me_round.png "../clustering/algoritmo/me_round.png"
+popd
+popd
+popd
+
+:: Run pandoc
 pandoc -t beamer -V theme:Darmstadt --slide-level=2 --variable fontsize=9pt --toc -o slides.pdf ^
 introduccion.md ^
 k-means.md ^
 bfr.md ^
-cure.md
+cure.md ^
+../../common/me.md
 
 
 :: themes:  Antibes Bergen Berkeley Berlin
